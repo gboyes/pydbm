5d03cfdc7c3132913a03209930fe750d37756921
graham boyes <gr.ed.boyes@gmail.com>
2011-12-23 16:37:28 -0500

    minor changes

--- tests/constraint_test.py
+++ tests/constraint_test.py
@@ -18,5 +18,5 @@ x_ = np.zeros(len(x) + max(D.atoms['duration']))
 x_[0:len(x)] = x
 
 #min spacing is a quantizing element
-mod, res, M = D.mpc(x_.copy(), 1000, 35, 1, 2000)
+mod, res, M = D.mpc(x_.copy(), 1000, 35, 4, 500)
 audiolab.wavwrite(mod, '/home/frobenius/Desktop/mod.wav', fs)
